5c8fd3c5e8f75dc0f1123fd9f93a9f9df7a37936
Brian <bmh123@comcast.net>
2026-09-08 22:43:33 -0400

    Fixed bug with TURRET_COUNT not being used

--- src/main.c
+++ src/main.c
@@ -528,7 +528,7 @@ int main()
 
 					ParticlesDraw();
 
-					for (int i = 0; i < 8; i++)
+					for (int i = 0; i < TURRET_COUNT; i++)
 					{
 						UpdateTurret(&world, &turrets[i], GetFrameTime());
 						DrawTurret(&mdl_turret, &turrets[i]);
